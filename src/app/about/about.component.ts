d7170289f92715da4d36087fd87f4b0e42da48c0
OshaniKumarathunga <oshanipbkonline@gmail.com>
2022-12-22 19:02:24 +0530

    Angular Promises

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -14,7 +14,8 @@ export class AboutComponent implements OnInit {
   constructor(private leaderService: LeaderService) { }
 
   ngOnInit(): void {
-    this.leaders = this.leaderService.getLeaders();
+    this.leaderService.getLeaders()
+    .then(leaders => this.leaders = leaders);
   }
 
 }
