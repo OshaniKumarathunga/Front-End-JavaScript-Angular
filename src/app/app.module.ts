17b450c3d0a7d81b8c0ecb171854091c5fc0d0a3
OshaniKumarathunga <oshanipbkonline@gmail.com>
2022-12-23 00:08:37 +0530

    Angular Promises 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import 'hammerjs';
 
@@ -64,7 +65,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FormsModule,
     ReactiveFormsModule,
     MatSelectModule,
-    MatSlideToggleModule
+    MatSlideToggleModule, 
+    MatProgressSpinnerModule
   ],
   providers: [
     DishService , 
